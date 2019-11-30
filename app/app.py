4a1a376d2af30350042a07b96e956698c57dcd31
MetroSpinnin <jimsamuel50@gmail.com>
2019-11-30 14:55:46 -0800

    Fix: Added clear and extra desc for models

--- app/app.py
+++ app/app.py
@@ -6,22 +6,28 @@ from flask import render_template
 from flask import url_for
 from flask import request
 
-# Start the main even loop
-
+# Initialize the Application
 app = Flask(__name__)
 
-# Start calling basic routes
-
+# The function below renders the prediction 
+# template of the data which gives end-users
+# different options to select the required data
 @app.route('/')
 def prediction():
 	return render_template('predict.html')
 
+# The function initializes the numerical python module
+# to shape the vals into an array and load our Model in
+# the Pickle file
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,12)
     loaded_model = pickle.load(open("model.pkl","rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
 
+# This route actually checks if the form has been filled
+# and then returns the values of the form and pass it to
+# the Prediction template to retrieve results.
 @app.route('/result',methods = ['POST'])
 def result():
     if request.method == 'POST':
@@ -29,11 +35,16 @@ def result():
         to_predict_list=list(to_predict_list.values())
         to_predict_list = list(map(int, to_predict_list))
         result = ValuePredictor(to_predict_list)
+
+        # After the values has been checked by mapping 
+        # the values of the form from a dictionary to a
+        # List return 1 if condition is true or 0 if false
         if int(result)==1:
             prediction='Income more than 50K'
         else:
             prediction='Income less that 50K'
         return render_template("result.html",prediction=prediction)
 
+# Finally run the main event Loop of the Application
 if __name__ == '__main__':
 	app.run(debug=True)
